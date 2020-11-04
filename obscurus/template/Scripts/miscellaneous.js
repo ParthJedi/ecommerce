c4ab66676639bab260534fb92f0a579b35006b1c
Parth Joshi <parth.joshi@asianetnews.in>
2020-11-05 02:11:47 +0530

    html-commit#03

---
+++ obscurus/template/Scripts/miscellaneous.js
@@ -0,0 +1,7 @@
+function openAsideBar() {
+    document.querySelector('.side-bar').classList.add('open');    
+}
+
+function closeAsideBar() {
+    document.querySelector('.side-bar').classList.remove('open');
+}
