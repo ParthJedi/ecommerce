7c35b12e742241168997c0a7deaf9b17d68fa8b9
Parth Joshi <parth.joshi@asianetnews.in>
2020-11-06 00:35:39 +0530

    product listed dynamically

--- festrush/frontend/src/App.js
+++ festrush/frontend/src/App.js
@@ -1,6 +1,8 @@
 import './Style/style.css';
 import './Style/homescreen.css';
 import './Style/side-bar.css';
+import data from './data';
+
 
 function App() {
 
@@ -14,77 +16,57 @@ function App() {
 
   return (
     <div class="grid-container">
-        <header class="header">
-          <nav>
-              <div class="brand">
-                  <button onClick={openAsideBar}>
-                      &#9776;
+      <header class="header">
+        <nav>
+          <div class="brand">
+            <button onClick={openAsideBar}>
+              &#9776;
                   </button>
-              </div>
-              <div id="brand_name">Fest<span class="logo-break">rush</span></div>
-              <ul class="navbar">
-                  <li><a herf="shopping-cart.html" class="btn">Cart</a></li>
-                  <li><a herf="sign-in.html" class="btn">
-                      <span class="material-icons">account_circle</span>&nbsp;Sign In</a>
-                  </li>
-              </ul>                    
-          </nav>
-        </header>
-        <aside id="mySidebar" class="side-bar">
-            <h3>Check Our Categories</h3>
-            <button class="side-bar-close-btn" onClick={closeAsideBar}>X</button>
-            <ul>
-                <li><a href="#"><i class="material-icons">face</i><span class="icon-text">Shirt</span></a></li>
-                <li><a href="#"><i class="material-icons">directions_run</i><span class="icon-text">Pants</span></a></li>
-                <li><a href="#"><i class="material-icons">do_not_step</i><span class="icon-text">Shoes</span></a></li>
-            </ul>
-        </aside>
-        <main id="main" class="main">
-          <div class="content">
-            <ul class="products">
-              <li>
-                  <div class="product">
-                      <img class="product-image" src='/images/p2.jpg' alt="product" />                                
-                      <div class="product-name"><a href="product.html">Om Namo Shivaya</a></div>
-                      <div class="product-brand">Obscurus Apparels</div>
-                      <div class="product-price">$33</div>
-                      <div class="product-ratings">4.4 Stars (11 reviews)</div>                                
-                  </div>
-              </li>
-              <li>
-                  <div class="product">
-                      <img class="product-image" src='/images/p2.jpg' alt="product" />                                
-                      <div class="product-name"><a href="product.html">Om Namah Shivaya</a></div>
-                      <div class="product-brand">Obscurus Apparels</div>
-                      <div class="product-price">$33</div>
-                      <div class="product-ratings">4.4 Stars (11 reviews)</div>                                
-                  </div>
-              </li>
-              <li>
-                  <div class="product">
-                      <img class="product-image" src='/images/p2.jpg' alt="product" />                                
-                      <div class="product-name"><a href="product.html">Om Namah Shivaya</a></div>
-                      <div class="product-brand">Obscurus Apparels</div>
-                      <div class="product-price">$33</div>
-                      <div class="product-ratings">4.4 Stars (11 reviews)</div>                                
-                  </div>
-              </li>                 
-              <li>
-                  <div class="product">
-                      <img class="product-image" src='/images/p2.jpg' alt="product" />                                
-                      <div class="product-name"><a href="product.html">Om Namah Shivaya</a></div>
-                      <div class="product-brand">Obscurus Apparels</div>
-                      <div class="product-price">$33</div>
-                      <div class="product-ratings">4.4 Stars (11 reviews)</div>                                
-                  </div>
-              </li>
-            </ul>
           </div>
-        </main>
-        <footer class="footer">
-          <div>Fest<span class="logo-break">rush</span> &copy; 2020 <br/> All Rights Reserved</div>
-        </footer>    
+          <div id="brand_name">Fest<span class="logo-break">rush</span></div>
+          <ul class="navbar">
+            <li><a herf="shopping-cart.html" class="btn">Cart</a></li>
+            <li><a herf="sign-in.html" class="btn">
+              <span class="material-icons">account_circle</span>&nbsp;Sign In</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <aside id="mySidebar" class="side-bar">
+        <h3>Check Our Categories</h3>
+        <button class="side-bar-close-btn" onClick={closeAsideBar}>X</button>
+        <ul>
+          <li><a href="#"><i class="material-icons">face</i><span class="icon-text">Shirt</span></a></li>
+          <li><a href="#"><i class="material-icons">directions_run</i><span class="icon-text">Pants</span></a></li>
+          <li><a href="#"><i class="material-icons">do_not_step</i><span class="icon-text">Shoes</span></a></li>
+        </ul>
+      </aside>
+      <main id="main" class="main">
+        <div class="content">
+          <ul class="products">
+            {
+              data.products.map(product => {
+                return (
+                  <li>
+                    <div class="product">
+                      <img class="product-image" src={product.image} alt="product" />
+                      <div class="product-name"><a href="product.html">{product.name}</a></div>
+                      <div class="product-brand">{product.brand}</div>
+                      <div class="product-price">${product.price}</div>
+                      <div class="product-ratings">{product.rating} Stars (from {product.reviewsCount} reviews)</div>
+                    </div>
+                  </li>
+                );
+              })
+            }
+
+          </ul>
         </div>
+      </main>
+      <footer class="footer">
+        <div>Fest<span class="logo-break">rush</span> &copy; 2020 <br /> All Rights Reserved</div>
+      </footer>
+    </div>
   );
 }
 
---
+++ festrush/frontend/src/data.js
@@ -0,0 +1,38 @@
+const data = {
+    products: [
+        {
+            image: '/images/p2.jpg',
+            name: 'om namah shivaya',
+            brand: 'obscurus apparels',
+            price: 33,
+            rating: 4.2,
+            reviewsCount: 5
+        },
+        {
+            image: '/images/p2.jpg',
+            name: 'om namah shiva',
+            brand: 'obscurus apparels',
+            price: 33,
+            rating: 4.2,
+            reviewsCount: 5
+        },
+        {
+            image: '/images/p2.jpg',
+            name: 'om trayambakam',
+            brand: 'obscurus apparels',
+            price: 33,
+            rating: 4.2,
+            reviewsCount: 5
+        },
+        {
+            image: '/images/p2.jpg',
+            name: 'om namah shivaya',
+            brand: 'obscurus apparels',
+            price: 33,
+            rating: 4.2,
+            reviewsCount: 5
+        }
+    ]
+}
+
+export default data;
