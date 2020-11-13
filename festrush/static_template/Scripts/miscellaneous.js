f27981aefe50b21eda46d4e54aee4912bccb3417
Parth Joshi <parth.joshi@asianetnews.in>
2020-11-14 01:56:33 +0530

    temp work removed.

--- festrush/static_template/Scripts/miscellaneous.js
+++
@@ -1,7 +0,0 @@
-function openAsideBar() {
-    document.querySelector('.side-bar').classList.add('open');    
-}
-
-function closeAsideBar() {
-    document.querySelector('.side-bar').classList.remove('open');
-}
