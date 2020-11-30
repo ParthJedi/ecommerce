19c15a282a6865b8851c73f8a5d6a5793040d6e2
Parth Joshi <parth.joshi@asianetnews.in>
2020-11-30 07:12:43 +0530

    Mongodb integration -User models and routes.

---
+++ festrush/backend/routes/userRoute.js
@@ -0,0 +1,22 @@
+import express from "express";
+import User from "../models/userModel";
+
+const router = express.Router();
+
+router.get("/createadmin", async (req, res) => {
+    try {
+            const user = new User({
+            name: 'Parth Jedi',
+            email: 'parth.joshi@gmail.com',
+            password: 'Admin@123',
+            isAdmin: true
+        });
+
+        const newUser = await user.save();
+        res.send(newUser);
+    } catch(error) {
+        res.send({message: error.message});
+    }
+});
+
+export default router;
--- festrush/backend/server.js
+++ festrush/backend/server.js
@@ -1,8 +1,21 @@
 import express from 'express';
 import data from './data';
+import dotenv from 'dotenv';
+import config from './config';
+import mongoose from 'mongoose';
+import userRoute from './routes/userRoute';
+
+dotenv.config();
+
+const mongodbUrl = config.MONGODB_URL;
+mongoose.connect(mongodbUrl,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).catch(error => console.log(error));
 
 const app = express();
-
+app.use("/api/users", userRoute)
 // routes to handle
 app.get('/api/products', (req, res) => {
     res.send(data.products);
