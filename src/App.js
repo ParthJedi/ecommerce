8aa6a9056cae93a3d3ec74f8a2cbf3bdda1a9540
Parth Joshi <parth.00.joshi@gmail.com>
2023-04-28 16:02:20 +0530

    reviving old project

--- src/App.js
+++ src/App.js
@@ -11,48 +11,50 @@ import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/signIn-and-signUp/SignIn-and-SignUp.component';
 import { checkUserSession } from './redux/user/userActions';
 
-import {currentUserSelector} from './redux/user/user.selector';
+import { currentUserSelector } from './redux/user/user.selector';
 import { selectCollectionsForPreview } from './redux/shop/shop.selector';
 
 class App extends React.Component {
+	unsubscribeFromAuth = null;
 
-  unsubscribeFromAuth = null;
+	componentDidMount() {
+		const { checkUserSession } = this.props;
+		checkUserSession();
+	}
 
-  componentDidMount() {
-    const { checkUserSession } = this.props;
-    checkUserSession();
-  }
+	componentWillUnmount() {
+		this.unsubscribeFromAuth();
+	}
 
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
-
-  render() {
-    return (
-      <div className='App'>
-        <createGlobalStyle />
-        <Header />
-        <Switch>        
-          <Route exact path='/' component={HomePage}/>
-          <Route path='/shop' component={ShopPage}/>          
-          <Route exact path='/checkout' component={CheckoutPage}/>
-          <Route exact path='/sign-in' render={() =>
-            this.props.currentUser ?
-            (<Redirect to="/" />) 
-            : (<SignInAndSignUp/>)} />
-        </Switch>
-      </div>
-    )
-  };
+	render() {
+		return (
+			<div className='App'>
+				<createGlobalStyle />
+				<Header />
+				<Switch>
+					<Route exact path='/' component={HomePage} />
+					<Route path='/shop' component={ShopPage} />
+					<Route exact path='/checkout' component={CheckoutPage} />
+					<Route
+						exact
+						path='/sign-in'
+						render={() =>
+							this.props.currentUser ? <Redirect to='/' /> : <SignInAndSignUp />
+						}
+					/>
+				</Switch>
+			</div>
+		);
+	}
 }
 
-const mapStateToProps = state => ({
-  currentUser: currentUserSelector(state),
-  collectionsArray: selectCollectionsForPreview(state)
+const mapStateToProps = (state) => ({
+	currentUser: currentUserSelector(state),
+	collectionsArray: selectCollectionsForPreview(state)
 });
 
-const mapDispatchToProps = dispatch => ({
-  checkUserSession: () => dispatch(checkUserSession())
-})
+const mapDispatchToProps = (dispatch) => ({
+	checkUserSession: () => dispatch(checkUserSession())
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
